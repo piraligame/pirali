6895d9ec9619e0c39b3de4fe654a1d702fd22477
piraligame <piraligameofficial@gmail.com>
2026-04-23 23:30:26 +0400

    Update leaderboard.js

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -1,54 +1,5 @@
-'use strict';
-
-class Leaderboard {
-    constructor() {
-        this.scores = this.loadScores();
-    }
-
-    loadScores() {
-        const scores = localStorage.getItem('leaderboard');
-        return scores ? JSON.parse(scores) : {};
-    }
-
-    saveScores() {
-        localStorage.setItem('leaderboard', JSON.stringify(this.scores));
-    }
-
-    addScore(player, score) {
-        if (!this.scores[player] || this.scores[player] < score) {
-            this.scores[player] = score;
-        }
-        this.saveScores();
-    }
-
-    getRankedScores() {
-        return Object.entries(this.scores)
-            .sort(([,scoreA], [,scoreB]) => scoreB - scoreA)
-            .map(([player, score]) => ({ player, score }));
-    }
-
-    display() {
-        const rankedScores = this.getRankedScores();
-        const displayArea = document.getElementById('leaderboard');
-        displayArea.innerHTML = '';
-        rankedScores.forEach(({ player, score }, index) => {
-            const rank = index + 1;
-            displayArea.innerHTML += `<div>${rank}. ${player} - ${score}</div>`;
-        });
-    }
+function lbPlay(){
+  document.getElementById('lb-overlay').style.display='none';
+  document.getElementById('lb-play').style.display='none';
+  startGame();
 }
-
-// Example usage:
-const leaderboard = new Leaderboard();
-leaderboard.addScore('Player1', 50);
-leaderboard.addScore('Player2', 75);
-leaderboard.display();
-
-// Firebase migration ready
-// Uncomment to add Firebase integration
-// function saveToFirebase() {
-//     // Implementation for saving scores to Firebase
-// }
-// function loadFromFirebase() {
-//     // Implementation for loading scores from Firebase
-// }
